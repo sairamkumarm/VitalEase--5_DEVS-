dfbaabcba31f50efeaeb80cef9013c5ea18beb1b
sairamkumarm <sairamkumarm21@gmail.com>
2023-11-02 01:06:29 +0530

    fixed footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -2,7 +2,7 @@
 
 </script>
 
-<div class="grid-cols-2 w-screen bg-secondary-500 bottom-0 right-0 left-0 fixed">
+<div class="grid-cols-2 w-screen bg-secondary-500 bottom-0 right-0 left-0 ">
 <div class="info">sdf</div>
 <div class="details">sdf</div>
 </div>
